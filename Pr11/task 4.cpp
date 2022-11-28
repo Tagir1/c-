bdaac0350e181bb1c1659f631b1792c50744d23b
Tagir1 <91716564+Tagir1@users.noreply.github.com>
2022-11-28 12:25:00 +0200

    Create task 4.cpp

---
+++ Pr11/task 4.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <iomanip> 
+#include <cstdlib> 
+#include <ctime>
+using namespace std;
+
+int main() 
+{
+    int m=6;
+    int n=4;
+    int sum[n];
+    int arr[m][n];
+    
+    srand(time(NULL));
+    for(int i=0;i<n;i++)
+    {
+        sum[i] = 0;
+    }
+    
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            arr[i][j]= rand()%50;
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    
+    for(int i=0;i<m;i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            sum[j] += arr[i][j];
+        }
+    }
+    for(int j=0;j<n;j++)
+    {
+        cout << "suma stovpchika nomer " << j+1 << " dorivnue " << sum[j] << endl;
+    }
+}
