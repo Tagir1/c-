cf270ae61078cd6d9652c707a47fa9a51ca6523c
Tagir1 <91716564+Tagir1@users.noreply.github.com>
2023-04-10 10:48:34 +0300

    Create task 1.cpp

---
+++ semestr2/Pr1/task 1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main() 
+{
+    int vector[] = {3, -5, 7, 10, -4, 14, 5, 2, -13};
+    int n = sizeof(vector)/sizeof(vector[0]);
+    int max = *vector;
+    
+    for(int i=0; i<n; i++)
+    {
+        if(*(vector + i) > max)
+        {
+            max = *(vector + i);
+        }
+    }
+    
+    cout << "maximum arrey element: "<< max;
+}
